24ace08d43c52d541f489dd1e47d54b3fa42907a
Rahlback <rahlback@kth.se>
2021-03-24 20:56:52 +0100

    Added some docstrings
    Changed encoding of pixels to correct strings

--- image_converter.py
+++ image_converter.py
@@ -1,12 +1,57 @@
+"""Image converter
+
+This script takes an image and encodes it into a binary image
+specifically for an E-ink screen with 3 pixel color (Red, black, white).
+Also supports transparent pixels.
+
+Currently only accepts PNG files in the RGBA and RGB formats. All other
+formats are untested.
+
+Encoding table:
+White = 00
+Red = 01
+Black = 10
+Transparent = 11
+
+This script can also be imported as a module and contains the following
+functions:
+
+    * get_rgb_text - Returns the encoded string of a RGB value
+    * image_to_ascii - Returns the image fully encoded
+
+"""
+#
+#
+#
+#
+
+
+
+
+
 from PIL import Image
 
-# Load test image
-
 def get_rgb_text(rgb_value):
+    """Gets the two bit representation of a RGB value.
+
+    Parameters
+    ----------
+    rgb_value : tuple, array
+        RGB or RGBA value of a pixel
+
+    Returns
+    -------
+    string
+        dependent on RGB value.
+        White = "00"
+        Red = "01"
+        Black = "10"
+        Transparent = "11"
+    """
 
     if len(rgb_value) == 4:
         if rgb_value[3] == 0:
-            return " "
+            return "11"
         rgb_value = [rgb_value[0], rgb_value[1], rgb_value[2]]
 
     for index, value in enumerate(rgb_value):
@@ -16,11 +61,11 @@ def get_rgb_text(rgb_value):
             rgb_value[index] = 0
 
     if rgb_value == [255, 0, 0]: # Red
-        return "R"
+        return "01"
     elif 255 in rgb_value: # Anything that is not red, will be white
-        return " "
+        return "00"
     elif rgb_value == [0, 0, 0]: # Black
-        return "B"
+        return "10"
 
 def image_to_ascii(image):
     width, height = image.size
@@ -36,8 +81,12 @@ def image_to_ascii(image):
     print(string_builder)
 
 
-im = Image.open("./fonts/hejemma.png")
-print(im)
+def main():
+    im = Image.open("./fonts/hejemma.png")
+    print(im)
 
 
-image_to_ascii(im)
+    image_to_ascii(im)
+
+if __name__ == '__main__':
+    main()
